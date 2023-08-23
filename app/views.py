6140873077d34175424f35f897092b3653bd554e
Arter Tendean <arter.tendean.07@gmail.com>
2023-08-23 23:56:51 +0800

    Adding `product` models

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 
 def index(request):
-    return render(request, "index.html")
+    context = {}
+
+    products = Product.objects.all().order_by("-id")
+    if products:
+        context["products"] = products
+
+    return render(request, "index.html", context)
